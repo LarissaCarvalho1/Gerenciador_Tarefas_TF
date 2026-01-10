45c9cd745a539f1ee7d44845b8e3a1a0eafb7f08
Larissa Carvalho <carvalholarissa530@gmail.com>
2026-01-10 18:06:51 -0300

    feat: Cria menu de ações closes #1

---
+++ main.py
@@ -0,0 +1,28 @@
+def menu():
+    while True:
+        print("""
+            ===== GERENCIADOR DE TAREFAS =====
+            1 - Criar tarefa
+            2 - Listar tarefas
+            3 - Atualizar tarefa
+            4 - Excluir tarefa
+            0 - Sair
+        """)
+
+        opcao = input("Escolha uma opcao: ")
+
+        if opcao == "1":
+            criar_tarefa()
+        elif opcao == "2":
+            listar_tarefas()
+        elif opcao == "3":
+            atualizar_tarefa()
+        elif opcao == "4":
+            excluir_tarefa()
+        elif opcao == "0":
+            print("Encerrando o sistema...")
+            break
+        else:
+            print("Opção inválida.\n")
+
+menu()
